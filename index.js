dbe512727f3529e92d6ed1ec13fb67d969f13d16
Marquetos <marcosgof24@gmail.com>
2025-08-12 00:28:38 -0300

    Hola mundo js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-  // Esta línea está comentada. Deberías descomentarla para que funcione
-  // y cambiar el texto que se envía como respuesta.
-
-  //res.end('Poné lo que quieras acá');
+  res.end('Hola Mundo');
 });
 
 server.listen(3000, () => {
